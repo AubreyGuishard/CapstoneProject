c116a2966adfdbe7599084d386c17e756b2042f7
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-04-03 16:58:26 -0400

    Working on Friends list

--- frontend/src/components/MyFriendsList/MyFriendsList.jsx
+++ frontend/src/components/MyFriendsList/MyFriendsList.jsx
@@ -6,7 +6,7 @@ import MyFriends from '../MyFriends/MyFriends'
 const MyFriendsList = () => { 
 const [user, token] = useAuth()
 const [friendList, setFriendList] = useState([])
-const [friends, setFriends] = useState([])
+const [friends, setFriends] = useState([{}])
 const navigate = useNavigate
 
 const fetchDataFromAPI = async () => {
@@ -22,6 +22,51 @@ const fetchDataFromAPI = async () => {
 useEffect(() => {
     fetchDataFromAPI()
 }, []);
+
+async function toggleUserFriend(){
+    try{
+       let url = `http://127.0.0.1:8000/api/auth/user/follow/${attendee.id}/`
+       let response = await axios.patch(url, {user},
+           {
+               headers: {
+                   Authorization: "Bearer " + token
+               }
+           }
+           );
+           if (response.status === 201){
+               navigate("/myfriends/")
+           }
+       console.log(response)
+    }  catch(error){
+       console.log('Error Message', error.message);
+       console.log('Token', token)
+    }
+   }
+
+
+   function handleFriend(){
+    if (token){
+        toggleUserFriend()
+        setFriends(true)
+        fetchDataFromAPI(user)
+    }
+    else{
+        navigate('/login/')
+    }
+}
+
+function handleUnfriend(){
+    if (token){
+        toggleUserFriend()
+        setFriends(false)
+        fetchDataFromAPI(user)
+    }
+}
+
+useEffect(() => {
+    fetchDataFromAPI(user)
+    console.log('My Friends List', friends)
+}, [])
 
 return (<ul>
 {friends.map(item => (
